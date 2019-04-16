8149284ea8b542ab353ee35d91c0d0caedae628b
Anders Miltner <Anders.Miltner@gmail.com>
2019-04-16 13:19:33 -0400

    notes and fixes

--- code/src/MIG.ml
+++ code/src/MIG.ml
@@ -12,12 +12,70 @@ module MIGLearner(V : Verifier) = struct
       ~post:(post : UniversalFormula.t)
       ~positives:(positives : Value.t list)
     : Value.t option =
+    Log.info
+      (lazy ("Checking boundary for: " ^ Expr.show eval));
     V.true_on_examples
       ~problem
       ~examples:positives
       ~eval
       ~eval_t
       ~post
+
+  let satisfyTransAll
+      ~problem:(problem : problem)
+      ~invariant:(invariant : Expr.t)
+      ~positives:(positives : Value.t list)
+    : ((Expr.t,Value.t) either) =
+    let rec helper
+        (invariant : Expr.t)
+      : ((Expr.t,Value.t) either) =
+      let app_var = "x" in
+      let invariant_applied = Expr.mk_app invariant (Expr.mk_var app_var) in
+      let applied_arg = (app_var, Type.mk_var "t") in
+      let post = ([applied_arg],invariant_applied) in
+      let boundary_validity =
+        List.fold_left
+          ~f:(fun acc (eval,eval_t) ->
+              begin match acc with
+                | None ->
+                  push_boundary
+                    ~problem
+                    ~eval
+                    ~eval_t
+                    ~post
+                    ~positives
+                | Some _ -> acc
+              end)
+          ~init:None
+          problem.mod_vals
+      in
+      begin match boundary_validity with
+        | Some m ->
+          Log.info
+            (lazy ("Boundary Not Satisfied, counterexample:"
+                   ^ (Log.indented_sep 4)
+                   ^ (Value.show m))) ;
+          Right m
+        | None ->
+          Log.info
+            (lazy ("IND >> Strengthening for inductiveness:"
+                   ^ (Log.indented_sep 4)
+                   ^ (Expr.show invariant))) ;
+          let pre_inv =
+            VPIE.learnVPreCondAll
+              ~problem
+              ~pre:invariant
+              ~post
+              ~positives:positives
+          in
+          Log.debug (lazy ("IND Delta: " ^ (Expr.show pre_inv))) ;
+          if is_equal @$ Expr.compare pre_inv invariant then
+            Left invariant
+          else
+            helper pre_inv
+      end
+    in
+    helper invariant
 
   let satisfyTrans
       ~problem:(problem : problem)
@@ -106,6 +164,12 @@ module MIGLearner(V : Verifier) = struct
     (* terminates when either all have been processed and found invariant *)
     (* or until it becomes too strong, and a positive counterexample is added *)
     let inv_or_pos =
+      satisfyTransAll
+        ~problem
+        ~invariant:initial_invariant
+        ~positives
+    in
+    (*let inv_or_pos =
       fold_until_right_or_list_end
         ~f:(fun acc (e,t) ->
             satisfyTrans
@@ -116,7 +180,7 @@ module MIGLearner(V : Verifier) = struct
               ~positives)
         ~init:initial_invariant
         problem.mod_vals
-    in
+      in*)
     begin match inv_or_pos with
       | Left inv -> inv
       | Right ce -> restart_with_new_positives ce
--- code/src/VPIE.ml
+++ code/src/VPIE.ml
@@ -4,6 +4,89 @@ open Lang
 
 module Make(V : Verifier) =
 struct
+  let learnVPreCondAll
+      ~(problem : problem)
+      ~(pre : Expr.t)
+      ~(post : UniversalFormula.t)
+      ~(positives : Value.t list)
+    : Expr.t =
+    let testbed = TestBed.create_positive positives in
+    let rec helper
+        (attempt:int)
+        (testbed:TestBed.t)
+      : Expr.t =
+      (Log.info (lazy ("VPIE Attempt "
+                       ^ (Int.to_string attempt)
+                       ^ "."));
+       let pos_examples = List.map ~f:(fun (v,_) -> v) testbed.pos_tests in
+       let neg_examples = List.map ~f:(fun (v,_) -> v) testbed.neg_tests in
+       let subvalues =
+         List.concat_map
+           ~f:Value.strict_subvalues
+           (pos_examples@neg_examples)
+       in
+       let all_inside_examples =
+         List.filter
+           ~f:(fun e ->
+               Typecheck.type_equiv
+                 problem.tc
+                 (Type.mk_var "t")
+                 (Typecheck.typecheck_exp problem.ec problem.tc problem.vc (Value.to_exp e)))
+           subvalues
+       in
+       let testbed_temp =
+         List.fold_left
+           ~f:(fun tb e ->
+               if Option.is_some
+                   (V.true_on_examples
+                      ~problem
+                      ~examples:[e]
+                      ~eval:(Expr.mk_identity_func (Type.mk_var "t"))
+                      ~eval_t:(Type.mk_arr (Type.mk_var "t") (Type.mk_var "t"))
+                      ~post:post) then
+                 TestBed.add_neg_test_allow_dups ~testbed:tb e
+               else
+                 TestBed.add_pos_test_allow_dups ~testbed:tb e
+             )
+           ~init:testbed
+           all_inside_examples
+       in
+       let synthed_pre = Expr.simplify @$ Option.value_exn (V.synth ~problem ~testbed:testbed_temp) in
+       Log.info (lazy ("Candidate Precondition: " ^ (Expr.show synthed_pre)));
+       let full_pre = Expr.and_predicates pre synthed_pre in
+       let model_o =
+         List.fold_left
+           ~f:(fun acc (eval,eval_t) ->
+               begin match acc with
+                 | None ->
+                   V.implication_counter_example
+                     ~problem
+                     ~pre:full_pre
+                     ~eval
+                     ~eval_t
+                     ~post
+                 | Some _ -> acc
+               end)
+           ~init:None
+           problem.mod_vals
+       in
+       begin match model_o with
+         | None ->
+           Log.info (lazy ("Verified Precondition: " ^ (Expr.show synthed_pre)));
+           full_pre
+         | Some model ->
+           if (List.length model <> 1) then
+             failwith ("cannot do such functions yet: " ^ (string_of_list Value.show model))
+           else
+             helper
+               (attempt+1)
+               (TestBed.add_neg_test_disallow_dups
+                  ~testbed
+                  (List.hd_exn model))
+       end)
+    in
+    helper 0 testbed
+
   let learnVPreCond
       ~(problem:problem)
       ~(pre:Expr.t)
@@ -17,59 +100,67 @@ struct
         (attempt:int)
         (testbed:TestBed.t)
       : Expr.t =
-        (Log.info (lazy ("VPIE Attempt "
-                         ^ (Int.to_string attempt)
-                         ^ "."));
-         let pos_examples = List.map ~f:(fun (v,_) -> v) testbed.pos_tests in
-         let neg_examples = List.map ~f:(fun (v,_) -> v) testbed.neg_tests in
-         let subvalues =
-           List.concat_map
-             ~f:Value.strict_subvalues
-             (pos_examples@neg_examples)
-         in
-         let all_inside_examples =
-           List.filter
-             ~f:(fun e ->
-                 Typecheck.type_equiv
-                   problem.tc
-                   (Type.mk_var "t")
-                   (Typecheck.typecheck_exp problem.ec problem.tc problem.vc (Value.to_exp e)))
-             subvalues
-         in
-         let temp_testbed =
-           List.fold_left
-             ~f:(fun tb e ->
-                 if Option.is_some
-                     (V.true_on_examples
-                        ~problem
-                        ~examples:[e]
-                        ~eval:eval
-                        ~eval_t
-                        ~post:post) then
-                   TestBed.add_neg_test_allow_dups ~testbed:tb e
-                 else
-                   TestBed.add_pos_test_allow_dups ~testbed:tb e
-              )
-             ~init:testbed
-             all_inside_examples
-           in
-         let synthed_pre = Expr.simplify @$ Option.value_exn (V.synth ~problem ~testbed:temp_testbed) in
-         Log.info (lazy ("Candidate Precondition: " ^ (Expr.show synthed_pre)));
-         let full_pre = Expr.and_predicates pre synthed_pre in
-         begin match V.implication_counter_example ~problem ~pre:full_pre ~eval ~eval_t ~post with
-           | None ->
-             Log.info (lazy ("Verified Precondition: " ^ (Expr.show synthed_pre)));
-             full_pre
-           | Some model ->
-             if (List.length model <> 1) then
-               failwith ("cannot do such functions yet: " ^ (string_of_list Value.show model))
-             else
-               helper
-                 (attempt+1)
-                 (TestBed.add_neg_test_disallow_dups
-                    ~testbed
-                    (List.hd_exn model))
-           end)
+      (Log.info (lazy ("VPIE Attempt "
+                       ^ (Int.to_string attempt)
+                       ^ "."));
+       let pos_examples = List.map ~f:(fun (v,_) -> v) testbed.pos_tests in
+       let neg_examples = List.map ~f:(fun (v,_) -> v) testbed.neg_tests in
+       let subvalues =
+         List.concat_map
+           ~f:Value.strict_subvalues
+           (pos_examples@neg_examples)
+       in
+       let all_inside_examples =
+         List.filter
+           ~f:(fun e ->
+               Typecheck.type_equiv
+                 problem.tc
+                 (Type.mk_var "t")
+                 (Typecheck.typecheck_exp problem.ec problem.tc problem.vc (Value.to_exp e)))
+           subvalues
+       in
+       let testbed =
+         List.fold_left
+           ~f:(fun tb e ->
+               if Option.is_some
+                   (V.true_on_examples
+                      ~problem
+                      ~examples:[e]
+                      ~eval:(Expr.mk_identity_func (Type.mk_var "t"))
+                      ~eval_t:(Type.mk_arr (Type.mk_var "t") (Type.mk_var "t"))
+                      ~post:post) then
+                 TestBed.add_neg_test_allow_dups ~testbed:tb e
+               else
+                 TestBed.add_pos_test_allow_dups ~testbed:tb e
+             )
+           ~init:testbed
+           all_inside_examples
+       in
+       let synthed_pre = Expr.simplify @$ Option.value_exn (V.synth ~problem ~testbed:testbed) in
+       Log.info (lazy ("Candidate Precondition: " ^ (Expr.show synthed_pre)));
+       let full_pre = Expr.and_predicates pre synthed_pre in
+       let model_o =
+         V.implication_counter_example
+           ~problem
+           ~pre:full_pre
+           ~eval
+           ~eval_t
+           ~post
+       in
+       begin match model_o with
+         | None ->
+           Log.info (lazy ("Verified Precondition: " ^ (Expr.show synthed_pre)));
+           full_pre
+         | Some model ->
+           if (List.length model <> 1) then
+             failwith ("cannot do such functions yet: " ^ (string_of_list Value.show model))
+           else
+             helper
+               (attempt+1)
+               (TestBed.add_neg_test_disallow_dups
+                  ~testbed
+                  (List.hd_exn model))
+       end)
     in
     helper 0 testbed
 end
