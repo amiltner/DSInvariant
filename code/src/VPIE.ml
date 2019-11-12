03709b99a45409a794321f71b2a5ffb5a8bd68f8
Anders Miltner <Anders.Miltner@gmail.com>
2019-11-12 15:51:43 -0500

    undo change that actually didnt make things easier to explain

--- code/src/VPIE.ml
+++ code/src/VPIE.ml
@@ -113,13 +113,9 @@ module Make (V : Verifier.t) (S : Synthesizer.t) (L : LR.t) = struct
               (TestBed.add_pos_tests_safe ~testbed:tb new_positives))
         answer_lists
     in
-    let answer_lists_rev = List.rev answer_lists in
-    let answer_lists_rev =
-      List.take_while
-        ~f:(fun (e,tb,_) -> satisfies_testbed ~problem tb e)
-        answer_lists_rev
-    in
-    List.rev answer_lists_rev
+    List.filter
+      ~f:(fun (e,tb,_) -> satisfies_testbed ~problem tb e)
+      answer_lists
 
   let synth_new_inv
       ~(problem:Problem.t)
