5ec5f665b346f0cf24c0bf2fb33a525ca480924d
Anders Miltner <Anders.Miltner@gmail.com>
2019-11-12 14:29:27 -0500

    move stuff around

---
+++ code/src/MIG_Alternative.ml
@@ -0,0 +1,151 @@
+open Core
+
+module Make
+    (V : Verifier.t)
+    (S : Synthesizer.t)
+    (L : LR.t)
+= struct
+  module VPIE = VPIE_Alternative.Make(V)(S)(L)
+
+  let push_boundary
+      ~(problem : Problem.t)
+      ~eval:(eval : Expr.t)
+      ~(eval_t : Type.t)
+      ~post:(post : Value.t LR.condition)
+      ~positives:(positives : Value.t list)
+    : Value.t list * Value.t list =
+    Log.info
+      (lazy ("Checking boundary for:" ^ (DSToMyth.full_to_pretty_myth_string ~problem eval)));
+    (L.verifier
+       ~problem
+       eval_t
+       post
+       (LR.Set positives)
+       (Eval.evaluate_with_holes_basic ~tc:problem.tc ~eval_context:problem.eval_context eval))
+
+  let satisfyTransAll
+      ~problem:(problem : Problem.t)
+      ~invariant:(invariant : Expr.t)
+      ~positives:(positives : Value.t list)
+    : ((Expr.t,Value.t list) Core.Either.t) =
+    let check_boundary
+        (invariant:Expr.t)
+      : Value.t list * Value.t list =
+      let post =
+        LR.Predicate
+          (fun v ->
+             Value.equal
+               Value.mk_true
+               (Eval.evaluate_with_holes_basic
+                  ~eval_context:problem.eval_context
+                  ~tc:problem.tc
+                  (Expr.mk_app
+                     invariant
+                     (Value.to_exp v))))
+      in
+      List.fold_left
+        ~f:(fun acc (eval,eval_t) ->
+            begin match acc with
+              | ([],[]) ->
+                push_boundary
+                  ~problem
+                  ~eval
+                  ~eval_t
+                  ~post
+                  ~positives
+              | _ -> acc
+            end)
+        ~init:([],[])
+        problem.mod_vals
+    in
+    let rec helper
+        (invariant : Expr.t)
+      : ((Expr.t,Value.t list) Either.t) =
+      let post =
+        LR.Predicate
+          (fun v ->
+             Value.equal
+               Value.mk_true
+               (Eval.evaluate_with_holes_basic
+                  ~tc:problem.tc
+                  ~eval_context:problem.eval_context
+                  (Expr.mk_app
+                     invariant
+                     (Value.to_exp v))))
+      in
+      let pre_or_ce =
+        VPIE.learnVPreCondAll
+          ~problem
+          ~pre:invariant
+          ~post
+          ~positives:positives
+          ~checker:check_boundary
+      in
+      Log.info
+        (lazy ("IND >> Strengthening for inductiveness:"
+               ^ (Log.indented_sep 4)
+               ^ (DSToMyth.full_to_pretty_myth_string ~problem invariant))) ;
+      begin match pre_or_ce with
+        | First pre_inv ->
+          Log.debug (lazy ("IND Delta: " ^ (DSToMyth.full_to_pretty_myth_string ~problem  pre_inv))) ;
+          if Expr.equal pre_inv (Expr.mk_constant_true_func (Type._t)) then
+            First (Expr.and_predicates pre_inv invariant)
+          else
+            helper (Expr.and_predicates pre_inv invariant)
+        | Second m ->
+          Log.info
+            (lazy ("Boundary Not Satisfied, counterexample:"
+                   ^ (Log.indented_sep 4)
+                   ^ (List.to_string ~f:Value.show (snd m))
+                   ^ (Log.indented_sep 4)
+                   ^ "Comes from"
+                   ^ (Log.indented_sep 4)
+                   ^ (List.to_string ~f:Value.show (fst m)))) ;
+          Second (snd m)
+      end
+    in
+    helper invariant
+
+
+  let rec learnInvariant_internal
+      ~(problem : Problem.t)
+      ~(positives : Value.t list)
+      ~(attempt:int)
+    : Expr.t =
+    let restart_with_new_positives
+        (positive : Value.t list)
+      : Expr.t =
+      begin
+        Log.info (lazy ("Restarting inference engine. Attempt was "
+                        ^ (string_of_int attempt)
+                        ^ ".")) ;
+        learnInvariant_internal
+          ~problem
+          ~positives:(positive@positives)
+          ~attempt:(attempt+1)
+      end
+    in
+    (* find I => Q *)
+    let initial_invariant =
+      VPIE.learnVPreCond
+        ~problem
+        ~pre:(Expr.mk_constant_true_func problem.module_type)
+        ~eval:(Expr.mk_identity_func (Type._t))
+        ~eval_t:(Type.mk_arrow (Type._t) (Type._t))
+        ~post:problem.post
+        ~positives:positives
+    in
+    (* inductiveness checks and updates to invariants *)
+    (* terminates when either all have been processed and found invariant *)
+    (* or until it becomes too strong, and a positive counterexample is added *)
+    let inv_or_pos =
+      satisfyTransAll
+        ~problem
+        ~invariant:initial_invariant
+        ~positives
+    in
+    match inv_or_pos with
+    | First inv -> inv
+    | Second ce -> restart_with_new_positives ce
+
+end
---
+++ code/src/VPIE_Alternative.ml
@@ -0,0 +1,228 @@
+open Core
+
+module Make (V : Verifier.t) (S : Synthesizer.t) (L : LR.t) = struct
+  module RealVPIE = VPIE.Make(V)(S)(L)
+  let negs : Value.t list ref = ref []
+
+  let learnVPreCondAll
+      ~(problem : Problem.t)
+      ~(pre : Expr.t)
+      ~(post : Value.t LR.condition)
+      ~(positives : Value.t list)
+      ~(checker : Expr.t -> (Value.t list) * (Value.t list))
+    : (Expr.t,(Value.t list) * (Value.t list)) Either.t =
+    let rec helper
+        (attempt:int)
+        (testbed:TestBed.t)
+        (pres:Expr.t list)
+      : (Expr.t,Value.t list * Value.t list) Either.t =
+      (Log.info (lazy ("VPIE Attempt "
+                       ^ (Int.to_string attempt)
+                       ^ "."));
+       let pres =
+         begin match List.filter ~f:(RealVPIE.satisfies_testbed ~problem testbed) pres with
+           | [] ->
+             let subvalues =
+               List.concat_map
+                 ~f:Value.strict_subvalues
+                 (testbed.pos_tests@testbed.neg_tests)
+             in
+             let all_inside_examples =
+               List.filter
+                 ~f:(fun e ->
+                     Typecheck.type_equiv
+                       problem.tc
+                       (Type._t)
+                       (Typecheck.typecheck_exp problem.ec problem.tc problem.vc (Value.to_exp e)))
+                 subvalues
+             in
+             let testbed =
+               List.fold_left
+                 ~f:(fun tb e ->
+                     if not
+                         (List.is_empty
+                            (snd
+                               (L.verifier
+                                  ~problem
+                                  (Type.mk_arrow (Type._t) (Type._t))
+                                  post
+                                  (LR.Set [e])
+                                  (Value.from_exp_exn (Expr.mk_identity_func Type._t))))) then
+                       TestBed.add_neg_test ~testbed:tb e
+                     else
+                       (if TestBed.contains_test ~testbed:tb e then
+                          tb
+                        else
+                          TestBed.add_pos_test ~testbed:tb e))
+                 ~init:testbed
+                 all_inside_examples
+             in
+             Log.info (lazy "testbed");
+             Log.info (lazy (TestBed.show testbed));
+             List.map ~f:(fun e -> assert (RealVPIE.satisfies_testbed ~problem testbed e); Expr.simplify e) (snd (S.synth ~problem ~testbed:testbed))
+           | pres ->
+             pres
+         end
+       in
+       let synthed_pre = List.hd_exn pres in
+       Log.info (lazy ("Candidate Precondition: " ^ (DSToMyth.full_to_pretty_myth_string ~problem synthed_pre)));
+       let full_pre = Expr.and_predicates pre synthed_pre in
+       let boundary_ce =
+         checker full_pre
+       in
+       begin match boundary_ce with
+         | ([],[]) ->
+           let model_o =
+             List.fold_left
+               ~f:(fun acc (eval,eval_t) ->
+                   begin match acc with
+                     | [] ->
+                       Log.info (lazy ("verifying: " ^ DSToMyth.full_to_pretty_myth_string ~problem eval));
+                       fst
+                       (L.verifier
+                         ~problem
+                         eval_t
+                         post
+                         (LR.Predicate
+                            (fun v ->
+                               Value.equal
+                                 Value.mk_true
+                                 (Eval.evaluate_with_holes_basic
+                                         ~tc:problem.tc
+                                    ~eval_context:problem.eval_context
+                                    (Expr.mk_app
+                                       full_pre
+                                       (Value.to_exp v)))))
+                         (Eval.evaluate_with_holes_basic ~tc:problem.tc ~eval_context:problem.eval_context eval))
+                       (*V.implication_counter_example
+                         ~problem
+                         ~pre:full_pre
+                         ~eval
+                         ~eval_t
+                         ~post*)
+                     | _ -> acc
+                   end)
+               ~init:[]
+               problem.mod_vals
+           in
+           begin match model_o with
+             | [] ->
+               Log.info (lazy ("Verified Precondition: " ^ (DSToMyth.full_to_pretty_myth_string ~problem synthed_pre)));
+               First synthed_pre
+             | model ->
+               let new_negs =
+                 List.filter
+                   ~f:(fun n -> not (TestBed.contains_test ~testbed n))
+                   model
+               in
+               assert (List.length new_negs > 0);
+                 Log.info (lazy ("Add negative examples: " ^ (List.to_string ~f:Value.show model)));
+                 helper
+                   (attempt+1)
+                   (TestBed.add_neg_tests
+                      ~testbed
+                      new_negs)
+                   pres
+           end
+         | ce -> Second ce
+       end)
+    in
+    let testbed = TestBed.create_positive positives in
+    helper 0 testbed [Expr.mk_constant_true_func (Type._t)]
+
+  let learnVPreCond
+      ~(problem:Problem.t)
+      ~(pre:Expr.t)
+      ~(eval : Expr.t)
+      ~(eval_t : Type.t)
+      ~(post : UniversalFormula.t)
+      ~(positives : Value.t list)
+    : Expr.t =
+    let rec helper
+        (attempt:int)
+        (testbed:TestBed.t)
+      : Expr.t =
+       let pres =
+         begin match List.filter ~f:(RealVPIE.satisfies_testbed ~problem testbed) !RealVPIE.possibilities with
+           | [] ->
+             (Log.info (lazy ("Learning new precondition set.")));
+             let subvalues =
+               List.concat_map
+                 ~f:Value.strict_subvalues
+                 (testbed.pos_tests@testbed.neg_tests)
+             in
+             let all_inside_examples =
+               List.filter
+                 ~f:(fun e ->
+                     Typecheck.type_equiv
+                       problem.tc
+                       (Type._t)
+                       (Typecheck.typecheck_exp problem.ec problem.tc problem.vc (Value.to_exp e)))
+                 subvalues
+             in
+             let testbed =
+               List.fold_left
+                 ~f:(fun tb e ->
+                     if not
+                         (List.is_empty
+                            (V.true_on_examples
+                               ~problem
+                               ~examples:[e]
+                               ~eval:(Expr.mk_identity_func (Type._t))
+                               ~eval_t:(Type.mk_arrow (Type._t) (Type._t))
+                               ~post:post)) then
+                       TestBed.add_neg_test ~testbed:tb e
+                     else
+                       TestBed.add_pos_test ~testbed:tb e
+                   )
+                 ~init:testbed
+                 all_inside_examples
+             in
+             Log.info (lazy "testbed");
+             Log.info (lazy (TestBed.show testbed));
+             let pres = List.map ~f:Expr.simplify (snd (S.synth ~problem ~testbed:testbed))
+              in RealVPIE.possibilities := pres
+               ; pres
+           | pres ->
+             pres
+         end
+       in
+      (Log.info (lazy ("VPIE Attempt "
+                       ^ (Int.to_string attempt)
+                       ^ "."));
+       let synthed_pre = List.hd_exn pres in
+       Log.info (lazy ("Candidate Precondition: " ^ (DSToMyth.full_to_pretty_myth_string ~problem synthed_pre)));
+       let full_pre = Expr.and_predicates pre synthed_pre in
+       let model_o =
+         V.implication_counter_example
+           ~problem
+           ~pre:full_pre
+           ~eval
+           ~eval_t
+           ~post
+       in
+       begin match model_o with
+         | [] ->
+           Log.info (lazy ("Verified Precondition: " ^ (DSToMyth.full_to_pretty_myth_string ~problem synthed_pre)));
+           synthed_pre
+         | model ->
+           if (List.length model <> 1) then
+             failwith ("cannot do such functions yet: " ^ (List.to_string ~f:Value.show model))
+           else
+             let new_neg_example = List.hd_exn model in
+             (*negs := new_neg_example::!negs;*)
+             Log.info (lazy ("Add negative example: " ^ (Value.show new_neg_example)));
+             helper
+               (attempt+1)
+               (TestBed.add_neg_test ~testbed new_neg_example)
+       end)
+    in
+    let testbed = TestBed.create_positive positives in
+    let testbed =
+      List.fold
+        ~f:(fun testbed n -> TestBed.add_neg_test ~testbed n)
+        ~init:testbed
+        !negs
+    in
+    helper 0 testbed
+end
